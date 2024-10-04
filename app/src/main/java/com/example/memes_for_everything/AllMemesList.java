334e9cfb64125f9c94ffc601b46d2fd189b9d04f
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2024-10-04 21:33:16 +0700

    До 18

--- app/src/main/java/com/example/memes_for_everything/AllMemesList.java
+++ app/src/main/java/com/example/memes_for_everything/AllMemesList.java
@@ -11,6 +11,17 @@ public class AllMemesList {
             new MemeSound("Телефон","?", "Настроение", R.raw.a6, R.drawable.a6),
             new MemeSound("СЛАВЯНСКИЙ ЗАЖИМ ЯЙЦАМИ","Русь", "Русы и ящеры", R.raw.a7, R.drawable.a7),
             new MemeSound("Ох, зря я туда полез","КотБегемот", "Страшилки", R.raw.a8, R.drawable.a8),
+            new MemeSound("Не надо было это кушать","КотБегемот", "Страшилки", R.raw.a9, R.drawable.a9),
+            new MemeSound("Ура, я пидорас!","Дискорд", "Секс", R.raw.a10, R.drawable.a10),
+            new MemeSound("Нюхай бэбру","Бебра", "Дело в шляпе", R.raw.a11, R.drawable.a11),
+            new MemeSound("Kiss me again","Ютуб", "Анимешная тяночка, женщина", R.raw.a12, R.drawable.a12),
+            new MemeSound("Братишка, я тебе покушать принёс","Зелёный слоник", "зеленый слоник, говно", R.raw.a13, R.drawable.a13),
+            new MemeSound("Одна история охуительнее другой","Зелёный слоник", "зеленый слоник, говно", R.raw.a14, R.drawable.a14),
+            new MemeSound("Обосрался пидорас","Зелёный слоник", "зеленый слоник, говно", R.raw.a15, R.drawable.a15),
+            new MemeSound("Начальник блять!","Зелёный слоник", "зеленый слоник, говно", R.raw.a16, R.drawable.a16),
+            new MemeSound("Чисти чисти","Ютуб", "зеленый слоник, говно", R.raw.a17, R.drawable.a17),
+            new MemeSound("С нами лесбиянка и пансексуал","Ютуб", "с нами лесбиянка, гей, гендеры, пидорасы", R.raw.a18, R.drawable.a18),
+
             new MemeSound("Система поиска пидорасов","?", "Геи, прикол", R.raw.a27, R.drawable.a27)
     };
 }
