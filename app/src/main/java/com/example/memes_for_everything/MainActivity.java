08e9ff185cd24f22aa905c884b380bee23239d0c
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2024-10-06 00:47:15 +0700

    UI обновление

--- app/src/main/java/com/example/memes_for_everything/MainActivity.java
+++ app/src/main/java/com/example/memes_for_everything/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.memes_for_everything;
 
+import android.annotation.SuppressLint;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.Resources;
-import android.media.AudioAttributes;
 import android.media.MediaPlayer;
-import android.media.SoundPool;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -23,18 +20,23 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("StaticFieldLeak")
+    public static MainActivity THIS;
 
     private RecyclerView recyclerView;
     private MemeSoundAdapter adapter;
     private EditText editText;
-
-    public static MainActivity THIS;
     private Resources resourcesLoader;
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+    List<MemeSound> soundListAfterSearch = new ArrayList<>();
+    private int SortMode = 3;
+    private int Columns = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,28 +61,76 @@ public class MainActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
             @Override
             public void afterTextChanged(Editable editable) {
-                List<MemeSound> nigger = new ArrayList<>();
-
-                for (MemeSound meme: AllMemesList.All) {
-                    if (meme.filterChecked(editText.getText().toString())) {
-                        nigger.add(meme);
-                    }
-                }
-
-                adapter = new MemeSoundAdapter(MainActivity.this, nigger);
-
-                recyclerView.setAdapter(adapter);
+                search();
             }
         });
 
         THIS = this;
 
-        adapter = new MemeSoundAdapter(this, Arrays.asList(AllMemesList.All));
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, Columns));
+
+        findViewById(R.id.btnStop).setOnClickListener(l -> {
+            try {
+                mediaPlayer.stop();
+            }
+            catch (Exception e) { e.printStackTrace();}
+        });
+        findViewById(R.id.btnNoSort).setOnClickListener(l -> Sort(0));
+        findViewById(R.id.btnByName).setOnClickListener(l -> Sort(1));
+        findViewById(R.id.btnByCategory).setOnClickListener(l -> Sort(2));
+        findViewById(R.id.btnInitial).setOnClickListener(l -> Sort(3));
+        findViewById(R.id.btnLesserColumns).setOnClickListener(l -> {
+            if (Columns > 1) {
+                Columns--;
+                recyclerView.setLayoutManager(new GridLayoutManager(this, Columns));
+            }
+        });
+        findViewById(R.id.btnMoreColumns).setOnClickListener(l -> {
+            Columns++;
+            recyclerView.setLayoutManager(new GridLayoutManager(this, Columns));
+        });
+
+        search();
+
     }
 
-    MediaPlayer mediaPlayer = new MediaPlayer();
+    private void search() {
+        soundListAfterSearch = new ArrayList<>();
+        for (MemeSound meme: AllMemesList.All) {
+            if (meme.filterChecked(editText.getText().toString())) {
+                soundListAfterSearch.add(meme);
+            }
+        }
+        Sort(SortMode);
+    }
+
+    private boolean flagDoubleClick = false;
+    private void Sort(int mode) {
+
+        switch (mode) {
+            case 0:
+                Collections.shuffle(soundListAfterSearch); break;
+            case 1:
+                soundListAfterSearch.sort(Comparator.comparing(meme -> meme.Name)); break;
+            case 2:
+                soundListAfterSearch.sort(Comparator.comparing(meme -> meme.Source)); break;
+            default:
+                break;
+        }
+        if (flagDoubleClick) {
+            Collections.reverse(soundListAfterSearch);
+        }
+        adapter = new MemeSoundAdapter(MainActivity.this, soundListAfterSearch);
+        recyclerView.setAdapter(adapter);
+
+        if (SortMode != mode) {
+            SortMode = mode;
+            flagDoubleClick = false;
+        }
+        else if (!flagDoubleClick) {
+            flagDoubleClick = true;
+        }
+    }
 
     public void playSound(int soundId) {
         if (mediaPlayer == null) {
